f1eef189af57e1c331728d064a2242d7411b5a92
Jayanth Naramsetty <89616595+jayanthnaramsetty@users.noreply.github.com>
2022-03-03 10:47:26 -0600

    Add files via upload

---
+++ Naramsetty_SearchApp/Naramsetty_SearchApp/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  Naramsetty_SearchApp
+//
+//  Created by Naramsetty,Jayanth on 3/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    @IBOutlet weak var resultImage: UIImageView!
+    
+    @IBOutlet weak var topicInfoText: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func searchButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction func ShowPrevImagesBtn(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func showNextImagesBtn(_ sender: UIButton) {
+    }
+    
+    
+    
+}
+
